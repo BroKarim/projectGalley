ebcf6bc783aa6ad82788de5c9b4cf7caf2077832
kiramDZ <hilmi0@mhs.unsyiah.ac.id>
2023-10-26 21:46:35 +0700

    perubahan pada card

--- src/components/app/main-content/Card.jsx
+++ src/components/app/main-content/Card.jsx
@@ -5,7 +5,7 @@ export default function Card() {
     <section>
       <div className="container mx-auto mt-4">
         <div className='mb-8'>
-          <h1 className="text-3xl font-bold ">Library</h1>
+          <h1 className="text-3xl font-bold ">Projects</h1>
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
           {cards.map((card, index) => (
