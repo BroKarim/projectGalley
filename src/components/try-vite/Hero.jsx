bce4c7d2d2a20cc533e8444c8ae3966c794ff27d
kiramDZ <hilmi0@mhs.unsyiah.ac.id>
2023-11-05 16:44:23 +0700

    btn source code

--- src/components/try-vite/Hero.jsx
+++ src/components/try-vite/Hero.jsx
@@ -24,11 +24,11 @@ export default function Hero(){
             </h1>
             <p className="mt-6 text-md leading-1 text-gray-600">I am eager to leverage my full-stack development skills to craft user-friendly and intuitive websites with a modern and seamless user experience.</p>
             <div className="my-8 flex items-center justify-center gap-x-6">
-              <a href="#_" className="relative inline-block text-lg group">
+              <a href="https://github.com/BroKarim/Hero-section.git" target="_blank" className="relative inline-block text-lg group">
                 <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
                   <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
                   <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
-                  <span className="relative">Lets talk</span>
+                  <span className="relative">Source Code</span>
                 </span>
                 <span className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0" data-rounded="rounded-lg"></span>
               </a>
