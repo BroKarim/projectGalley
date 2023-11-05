cd901ed755a8a06a7aa983dc2b6fdf13af3522e9
kiramDZ <hilmi0@mhs.unsyiah.ac.id>
2023-11-05 15:05:32 +0700

    card ditekan pindah halaman

--- src/components/app/main-content/Card.jsx
+++ src/components/app/main-content/Card.jsx
@@ -9,7 +9,7 @@ export default function Card() {
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
           {cards.map((card, index) => (
-            <a href={card.link} className="card m-2 transform cursor-pointer rounded-lg border border-gray-400 transition-all duration-200 hover:-translate-y-1 hover:border-opacity-0 hover:shadow-md" key={index}>
+            <a href={card.link} target='_blank' className="card m-2 transform cursor-pointer rounded-lg border border-gray-400 transition-all duration-200 hover:-translate-y-1 hover:border-opacity-0 hover:shadow-md" key={index}>
               <div className="m-3">
                 <h2 className="mb-2 text-lg">
                   {card.title} <span className="float-right inline animate-pulse rounded-full bg-teal-100 px-2 align-top font-mono text-sm text-teal-800">{card.tag}</span>
