668393ad5ad6f7049dc68309655e2c21a3c47d4e
kiramDZ <hilmi0@mhs.unsyiah.ac.id>
2023-12-23 12:06:37 +0700

    add link ig

--- src/components/app/main-content/Footer.jsx
+++ src/components/app/main-content/Footer.jsx
@@ -1,7 +1,12 @@
 export default function Footer() {
   return (
     <footer className="px-4 divide-y bg-black text-white font-mono">
-      <div className="py-6 text-sm text-center ">Create with 💓 by karim</div>
+      <div className="py-6 text-sm text-center">
+        Create with 💓 {' '}
+        <a href="https://www.instagram.com/brokariim/" className="cursor-pointer hover:border-b hover:border-white " target="_blank" rel="noopener noreferrer">
+        by karim
+        </a>
+      </div>
     </footer>
   );
 }
