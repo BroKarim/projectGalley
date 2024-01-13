5f7eb843f6eab03d36f2282db3c45bfa1d80a8d5
kiramDZ <hilmi0@mhs.unsyiah.ac.id>
2024-01-13 11:05:37 +0700

    update new github repo link

--- src/components/app/navigation/Navbar.jsx
+++ src/components/app/navigation/Navbar.jsx
@@ -12,7 +12,7 @@ export default function Navbar() {
             <RealTimeCopy></RealTimeCopy>
           </div>
           <div>
-            <a href="https://github.com/BroKarim" target='_blank' className="relative inline-block px-4 mx-2 py-2 font-medium group">
+            <a href="https://github.com/BroKarim/BroKarim-Project" target='_blank' className="relative inline-block px-4 mx-2 py-2 font-medium group">
               <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
               <span className="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
               <span className="relative text-black group-hover:text-white">My Github</span>
