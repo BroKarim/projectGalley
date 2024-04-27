45f163861ae4766791f3a3fd0abe97b96ff1f086
kiramDZ <hilmi0@mhs.unsyiah.ac.id>
2024-04-27 11:42:39 +0700

    update info me

--- src/components/app/main-content/Information.jsx
+++ src/components/app/main-content/Information.jsx
@@ -8,10 +8,15 @@ export default function Information() {
   return (
     <section className="border-t-2 border-[#272829]">
       <div className="md:flex block p-8 justify-between">
-        <div className="w-1/4 text-md mb-4 md:border-none border-b-2 border-[#19A7CE]">Information</div>
+        <div className="w-1/4 text-md mb-4 md:border-none border-b-2 border-[#19A7CE]">
+          Hi👋
+          <br />
+          Iam Karim
+        </div>
         <div className="flex max-w-[600px] ">
           <h3 className=" md:text-xl text-sm items-start mb-4">
-            Project gallery is a website that provide simple front-end project based on my mood, cause whenenver i see a new tech, new tool or front-end dev topic i can't stop my feeling to explore it"
+            a passionate front-end developer with a focus on user-centric design. I leverage React and Vite for a smooth front-end experience, coupled with Express.js on the back-end. Let's collaborate on building beautiful and functional
+            interfaces!
           </h3>
         </div>
         <div className="md:pl-20 flex flex-col gap-4 w-1/3 ">
@@ -24,7 +29,6 @@ export default function Information() {
             <p>{countCards()}</p>
           </div>
         </div>
-
       </div>
     </section>
   );
